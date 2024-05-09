acfc7ac899f7e1d39b60b83b9526a9ed265f9b89
Kea <LGTKEA001@myuct.ac.za>
2024-05-09 03:43:38 +0200

    fix: restructured

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -6,19 +6,54 @@ def validPassword(string):
         return True
     else:
         return False
+    
+def checkIntInput(input):
+        try:
+            intValue = int(input)
+            return intValue
+        except ValueError:
+            return -1
 
-print("Welcome to the PASSWORD GENERATOR")
-passwordLength = int(input("Specify the desired length of the password: "))
 
-randomSpecial = ""
-specials ="!@#$%&*_.><~"
-for k in range(passwordLength):
-    randomSpecial = randomSpecial + random.choice(string.ascii_letters + specials + string.digits)
-
-while validPassword(randomSpecial) == False and randomSpecial.find(specials) == -1 :
-    randomSpecial=""
-    for v in range(passwordLength):
+def passwordGen(passLength):
+    
+    randomSpecial = ""
+    specials ="!@#$%&*_.><~"
+    for k in range(passLength):
         randomSpecial = randomSpecial + random.choice(string.ascii_letters + specials + string.digits)
-else :
-    print("Your new password is:", "'" +randomSpecial+ "'")
+
+    while validPassword(randomSpecial) == False and randomSpecial.find(specials) == -1 :
+        randomSpecial=""
+        for v in range(passLength):
+            randomSpecial = randomSpecial + random.choice(string.ascii_letters + specials + string.digits)
+    else :
+        output ="Your new password is:" + "'" +randomSpecial+ "'"
+        return output
+
+
+print("Welcome to the PASSWORD GENERATOR" + "\n")
+
+def display():
+    passwordLength = input("Specify the desired length of the password: ")
+    result = checkIntInput(passwordLength)
+    if result == -1 :
+        print("Invalid entry. Please enter a numeric value.")
+    else:
+        ans = passwordGen(result)
+        print(ans)
+
+display()
+isExit= False
+while isExit==False:
+        reCalc = input("\n" + "Would you like to generate another password?" + "\n" +  "yes - (1) OR no - (0): ")
+        intRecalc = checkIntInput(reCalc)
+        if intRecalc == 1:
+            display()
+        elif intRecalc == 0:
+            print("You have choosen to exit." + "\n" +
+            "Bye!")
+            isExit=True
+            exit()
+        else:
+            print("\n" + "You have made an invalid selection. Please enter either (1) OR (0)" + "\n" )
 
